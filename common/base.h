6b2980760955df34052bec9511a205456bb25d66
ruvendix <ruvendix@naver.com>
2018-08-12 00:15:39 +0900

    common 필독 내용 추가
    추가된 내용 및 변경된 내용에 관한 것을 필독 내용에 추가했습니다.

--- common/base.h
+++ common/base.h
@@ -20,6 +20,11 @@
 #include <cstring>
 #include <clocale>
 
+// SDKDDKVer.h를 포함하면 최고 수준의 가용성을 가진 Windows 플랫폼이 정의됩니다.
+// 이전 Windows 플랫폼에 대해 응용 프로그램을 빌드하려는 경우에는 SDKDDKVer.h를 포함하기 전에
+// WinSDKVer.h를 포함하고 _WIN32_WINNT 매크로를 지원하려는 플랫폼으로 설정하십시오.
+#include <SDKDDKVer.h>
+
 // 불필요한 내용을 포함하지 않기 위해서 사용합니다.
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
